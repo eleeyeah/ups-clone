5701ca4a68d81f6d719c87fc52c0d0009a5dd3b1
admin <you@example.com>
2022-09-27 14:03:42 +0200

    custom hooks to pull the data

--- typings.d.ts
+++ typings.d.ts
@@ -7,3 +7,37 @@ type CustomerList = {
     name: ID;
     value: Customer;
 };
+
+type trackingItem = {
+    customer_id: ID;
+    customer: Customer;
+    items: Item[];
+};
+
+type Item = {
+    item_id: ID;
+    name: string;
+    price: number;
+    quantity: number;
+};
+
+type OrderResponse = {
+    value: Order;
+};
+
+type CustomerResponse = {
+    name: ID;
+    value: Customer;
+};
+
+type Order = {
+    carrier: string;
+    createdAt: string;
+    trackingId: string;
+    trackingItems: trackingItem;
+    shippingCost: number;
+    Address: string;
+    City: string;
+    Lng: number;
+    Lat: number;
+};
